279b8ecc6390eb4adb59d8ac1b47668cf68615c9
therazix <therazix@gmail.com>
2023-03-07 18:37:50 +0100

    Optimized getter and setter in Play class

--- TheatricalPlayersRefactoringKata/Play.cs
+++ TheatricalPlayersRefactoringKata/Play.cs
@@ -2,15 +2,13 @@ namespace TheatricalPlayersRefactoringKata
 {
     public class Play
     {
-        private string _name;
-        private string _type;
+        public string Name { get; set; }
+        public string Type { get; set; }
 
-        public string Name { get => _name; set => _name = value; }
-        public string Type { get => _type; set => _type = value; }
-
-        public Play(string name, string type) {
-            this._name = name;
-            this._type = type;
+        public Play(string name, string type)
+        {
+            Name = name;
+            Type = type;
         }
     }
 }
