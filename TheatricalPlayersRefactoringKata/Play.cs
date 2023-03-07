f2bea8303fc5d64292b8fe7024abdc0eab8fc66b
Jana Stopková <jankastopkova@seznam.cz>
2023-03-07 19:41:31 +0100

    Add constants to Play class

--- TheatricalPlayersRefactoringKata/Play.cs
+++ TheatricalPlayersRefactoringKata/Play.cs
@@ -5,10 +5,34 @@ namespace TheatricalPlayersRefactoringKata
         public string Name { get; set; }
         public string Type { get; set; }
 
+        private int _baseAmount;
+        private int _audienceThreshold;
+        private int _basePerAudience;
+        
+        private int _bonusBase;
+        private int _bonusPerAudience;
+
         public Play(string name, string type)
         {
             Name = name;
             Type = type;
+
+            if (type == "tragedy")
+            {
+                _baseAmount = 40000;
+                _audienceThreshold = 30;
+                _basePerAudience = 0;
+                _bonusBase = 0;
+                _bonusPerAudience = 1000;
+            }
+            else if (type == "comedy")
+            {
+                _baseAmount = 30000;
+                _audienceThreshold = 20;
+                _basePerAudience = 300;
+                _bonusBase = 10000;
+                _bonusPerAudience = 500;
+            }
         }
     }
 }
